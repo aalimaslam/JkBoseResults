da6391af0e21f203b9b14d4e5f34714d52de1671
Aalim aslam <67648366+aalimaslam@users.noreply.github.com>
2021-01-07 00:34:58 +0530

    Update prank.js

--- prank.js
+++ prank.js
@@ -1,4 +1,4 @@
 setTimeout(()=>{
   alert('You Have Been Pranked') ;
-  alert('Concept By Umaid\nDid By Aalim\n Marks By Umaid with Grades')
+  alert('Concept By Umaid\nDid By Aalim\nMarks By Umaid with Grades')
 },5000)

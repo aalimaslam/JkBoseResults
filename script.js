bff44b16bda6c6c894a6975c9496ebe8f0b6e4dd
Aalim aslam <67648366+aalimaslam@users.noreply.github.com>
2021-01-07 00:19:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const button = document.getElementById('check');
 form.addEventListener('submit',(e)=>{
     e.preventDefault()
     button.addEventListener('click',()=>{  
-    if(input.value = '18129677'){
+    if(input.value = 18129677){
         window.open('./Umaid.html');
         input.value = '';
     }

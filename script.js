b91cb3cde1e5edf406d5e6b6367e125dc5e8dc99
Aalim aslam <67648366+aalimaslam@users.noreply.github.com>
2021-01-07 00:12:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const form = document.getElementById('form');
 form.addEventListener('submit',(e)=>{
     e.preventDefault()
     if(input.value = '18129677'){
-        window.open('/Untitledumaid.png');
+        window.open('/Umaid.html');
         input.value = '';
     }
 })

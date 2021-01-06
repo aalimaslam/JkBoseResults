0f2bd0d2a12f7a684e8423b24b41409255ffd2e2
Aalim aslam <67648366+aalimaslam@users.noreply.github.com>
2021-01-07 00:10:24 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const input = document.getElementById('Id');
+const form = document.getElementById('form');
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    if(input.value = '18129677'){
+        window.open('/Untitledumaid.png');
+        input.value = '';
+    }
+})

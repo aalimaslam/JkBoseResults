906ce9692337342c56e83e068bb07a53f981f835
Aalim aslam <67648366+aalimaslam@users.noreply.github.com>
2021-01-07 17:48:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
 const input = document.getElementById('Id');
-const form = document.getElementById('form2');
 const button = document.getElementById('check');
 setTimeout(()=>{
 const aud = document.getElementById('audio');
     aud.play();
 },3000)
-
-form.addEventListener('submit',(e)=>{
-    e.preventDefault()
     button.addEventListener('click',()=>{  
-    if(input.value == '18129677'){
         window.open('./Umaid.html');
-        input.value = '';
-    }
-        else if (input.value =='hira roll no'){
-            window.open('./hira.html')
-            input.value = '';
-        }
-        else if(input.value == '18140301'){
-        window.open('./aisha.html');
-            input.value = '';
-        }
-        else{
-        alert('No Result Found');
-        }
+                if(input.value == '18129677'){
+                    window.open('./Umaid.html');
+                    input.value = '';
+                }
+                    else if (input.value =='hira roll no'){
+                        window.open('./hira.html')
+                        input.value = '';
+                    }
+                    else if(input.value == '18140301'){
+                    window.open('./aisha.html');
+                        input.value = '';
+                    }
+                    else{
+                    alert('No Result Found');
+                    }
     })
-})

346e84dd644046378e1b9f4b4d5192d8db3a3de8
Aalim aslam <67648366+aalimaslam@users.noreply.github.com>
2021-01-07 16:23:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const input = document.getElementById('Id');
 const form = document.getElementById('form2');
 const button = document.getElementById('check');
+setTimeout(()=>{
+const aud = document.getElementById('audio');
+    aud.play();
+},3000)
+
 form.addEventListener('submit',(e)=>{
     e.preventDefault()
     button.addEventListener('click',()=>{  

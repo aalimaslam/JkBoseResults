c9a830aabd7f7621089f819ba33d812c06c9bd40
Aalim aslam <67648366+aalimaslam@users.noreply.github.com>
2021-01-07 16:19:43 +0530

    Update prank.js

--- prank.js
+++ prank.js
@@ -1,6 +1,7 @@
+ const audio=getElementById('audio');
 setTimeout(()=>{
   alert('You Have Been Pranked') ;
   alert('Concept By Umaid\nDid By Aalim\nMarks By Umaid with Grades');
-  const audio=getElementById('audio');
+ 
   audio.play();
 },5000)
